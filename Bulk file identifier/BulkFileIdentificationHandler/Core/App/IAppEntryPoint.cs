11bcbdb7ab167305e3afce3c6b9718927c4d81a4
hasitha-mdosl <78337131+hasitha-mdosl@users.noreply.github.com>
2025-02-21 18:42:58 +0530

    Delete Bulk file identifier directory
    Public

--- Bulk file identifier/BulkFileIdentificationHandler/Core/App/IAppEntryPoint.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace BulkFileIdentificationHandler.Core.App
-{
-    public interface IAppEntryPoint
-    {
-        Task<object> Run(object input);
-    }
-}
--- Bulk file identifier/MDO2.Core.LMD/S3/Model/IS3UploadResponse.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace MDO2.Core.LMD.S3.Model
-{
-    public interface IS3UploadResponse : IS3Response
-    {
-    }
-}
